c1f8605a7dd1b76f585956d0809355eee05e0caf
Chahine Bendjeddou <chahine.bendjeddou99@gmail.com>
2023-06-28 03:47:33 +0100

    create the typings for tha data that will comme from the news api (stepZen)

--- typings.d.ts
+++ typings.d.ts
@@ -6,3 +6,28 @@ type Category =
   | 'science'
   | 'sports'
   | 'technology'
+
+type NewsResponse = {
+  pagination: Pagination
+  data: Article[]
+}
+
+type Pagination = {
+  count: Int
+  limit: Int
+  offset: Int
+  total: Int
+}
+
+type Article = {
+  author: string | null
+  category: string
+  country: string
+  description: string
+  image: string | null
+  language: string
+  published_at: string
+  source: string
+  title: string
+  url: string
+}

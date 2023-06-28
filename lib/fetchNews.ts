55f64084e1ee9c2efbac7bc10fd1b5be16c67297
Chahine Bendjeddou <chahine.bendjeddou99@gmail.com>
2023-06-28 05:06:56 +0100

    fetch the news from the stepzen api

---
+++ lib/fetchNews.ts
@@ -0,0 +1,69 @@
+import { gql } from 'graphql-request'
+import sortNewsByImage from './sortNewsByImage'
+
+const fetchNews = async (
+  category?: Category | string,
+  keywords?: string,
+  isDynamic?: boolean
+) => {
+  const query = gql`
+    query MyQuery(
+      $access_key: String!
+      $categories: String!
+      $keywords: String
+    ) {
+      myQuery(
+        access_key: $access_key
+        categories: $categories
+        keywords: $keywords
+        countries: "gb,sa,eg,ae,us,fr"
+        sort: "published_desc"
+      ) {
+        data {
+          author
+          category
+          country
+          description
+          image
+          language
+          published_at
+          source
+          title
+          url
+        }
+        pagination {
+          count
+          limit
+          offset
+          total
+        }
+      }
+    }
+  `
+
+  const res = await fetch(
+    'https://natuba.stepzen.net/api/wishing-gecko/__graphql',
+    {
+      method: 'POST',
+      cache: isDynamic ? 'no-cache' : 'default',
+      next: isDynamic ? { revalidate: 0 } : { revalidate: 20 },
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Apikey ${process.env.STEPZEN_API_KEY}`,
+      },
+      body: JSON.stringify({
+        query,
+        variables: {
+          access_key: process.env.MEDIASTACK_API_KEY,
+          categories: category,
+          keywords: keywords,
+        },
+      }),
+    }
+  )
+  const newsResponse = await res.json()
+  const news = sortNewsByImage(newsResponse.data.myQuery)
+  return news
+}
+
+export default fetchNews
